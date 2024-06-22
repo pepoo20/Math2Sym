179cea5f221b17728eede03a08e41ead6eac9a71
pepoo20 <85941953+pepoo20@users.noreply.github.com>
2024-06-23 01:51:49 +0700

    Remove redundant prompt templates and add in the new prompt about probability.

--- src/llmtuner/data/template.py
+++ src/llmtuner/data/template.py
@@ -914,73 +914,10 @@ _register_template(
     format_system=StringFormatter(slots=["{{content}}\n"]),
     format_separator=EmptyFormatter(slots=["\n"]),
 )
-
 _register_template(
-    name="MathTeacherPrompt",
-    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
-    format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
+    name = "WordProblemProbability",
+    format_user=StringFormatter(slots=["Question: {{content}} \n Answer: "]),
+    format_system=StringFormatter(slots=["{{content}}\n"]),
     format_separator=EmptyFormatter(slots=["\n"]),
-    stop_words=["<|im_end|>"],
-    replace_eos=True,
-    default_system=Math_Teacher_Prompt,
-)
-
-_register_template(
-    name="MathTeacherPromptWord",
-    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
-    format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
-    format_separator=EmptyFormatter(slots=["\n"]),
-    stop_words=["<|im_end|>"],
-    replace_eos=True,
-    default_system=Math_Teacher_Prompt_Word,
-)
-
-_register_template(
-    name="llama3_empty",
-    format_user=StringFormatter(
-        slots=[
-            (
-                "<|start_header_id|>user<|end_header_id|>\n\n{{content}}<|eot_id|>"
-                "<|start_header_id|>assistant<|end_header_id|>\n\n"
-            )
-        ]
-    ),
-    format_system=StringFormatter(
-        slots=[{"bos_token"}, "<|start_header_id|>system<|end_header_id|>\n\n{{content}}<|eot_id|>"]
-    ),
-    format_observation=StringFormatter(
-        slots=[
-            (
-                "<|start_header_id|>tool<|end_header_id|>\n\n{{content}}<|eot_id|>"
-                "<|start_header_id|>assistant<|end_header_id|>\n\n"
-            )
-        ]
-    ),
-    stop_words=["<|eot_id|>"],
-    replace_eos=True,
-)
-_register_template(
-    name="llama3_Math",
-    format_user=StringFormatter(
-        slots=[
-            (
-                "<|start_header_id|>user<|end_header_id|>\n\nQuestion: {{content}}\n Answer:<|eot_id|>"
-                "<|start_header_id|>assistant<|end_header_id|>\n\n"
-            )
-        ]
-    ),
-    format_system=StringFormatter(
-        slots=[{"bos_token"}, "<|start_header_id|>system<|end_header_id|>\n\n{{content}}<|eot_id|>"]
-    ),
-    format_observation=StringFormatter(
-        slots=[
-            (
-                "<|start_header_id|>tool<|end_header_id|>\n\n{{content}}<|eot_id|>"
-                "<|start_header_id|>assistant<|end_header_id|>\n\n"
-            )
-        ]
-    ),
-    stop_words=["<|eot_id|>"],
-    replace_eos=True,
-    default_system="You are a Math Teacher.Your goal is to understand a math word problem. Then recognize and distinguish which problem it is and then define the variables (if needed) and formulate the problem as it kind then transform it to Symbolic Form."
+    default_system="You are a Math Teacher specializing in probability and combinatorics. Your goal is to understand a math word problem,Identify the problem type to formulate the problem as it is and then transform it to Symbolic Form.",
 )
