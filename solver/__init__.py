88845bfaa4d7c26038ca63ed295e537d655a544c
pepoo20 <85941953+pepoo20@users.noreply.github.com>
2024-08-16 20:57:19 +0700

    Add Solver module to the project

---
+++ solver/__init__.py
@@ -0,0 +1,4 @@
+from .solve_main import extract_equation
+
+
+__all__ = ['extract_equation']
---
+++ solver/solve_function.py
@@ -0,0 +1,75 @@
+
+from sympy import symbols, solveset, S, Eq, solve, solve_univariate_inequality
+import sympy
+Relation = ['<', '>', '<=', '>=', '=']
+def GetEquationAndSymbol(Equation_to_Solve):
+    Equation = Equation_to_Solve[0]
+    Symbol = []
+    symbol_appear = 0
+    have_rational = False
+    if any(relation in str(Equation) for relation in Relation):
+        have_rational = True
+    if all(relation not in Equation_to_Solve for relation in Relation):
+        # print("Relation not in Equation_to_Solve")
+        for nums,expr in enumerate(Equation_to_Solve[1:]):
+            if len(str(expr)) == 1:
+                if str(expr).isdigit():
+                    Equation = Eq(Equation, expr)
+                else:
+                    if isinstance(expr, sympy.Symbol):
+                        if expr in Symbol:
+                            Equation = Eq(Equation, expr)
+                            symbol_appear +=1
+                        else:
+                            Symbol.append(expr)
+                            symbol_appear = nums
+            else:
+                Equation = Eq(Equation, expr)
+    else:
+        # print("Relation in Equation_to_Solve")
+        have_rational = True
+        Equation = ' '.join([str(elem) for elem in Equation_to_Solve[:-1]]) 
+        Symbol = Equation_to_Solve[-1]
+    return Equation, Symbol, have_rational, 
+
+def SystemLinearMoreThanTwo(Equation_to_Solve):
+    equation_list = []
+    symbol_list = []
+    for nums,expr in enumerate(Equation_to_Solve):
+        if isinstance(expr, sympy.Symbol):
+            if expr in symbol_list:
+                equation_list.append(expr)
+                symbol_appear +=1
+            else:
+                symbol_list.append(expr)
+                symbol_appear = nums
+        else:
+            equation_list.append(expr)
+    return solve(equation_list, symbol_list)
+
+def SolveLeastThan2Symbol(Equation_to_Solve):
+    Equation, Symbol, have_rational = GetEquationAndSymbol(Equation_to_Solve)
+    if isinstance(Equation, sympy.Eq):
+        Equation = Equation.args
+    if (Equation == '' or Equation == [] or Equation == None or Equation == False):
+        return "No result found"
+    # print(Equation, Symbol, have_rational)
+    if have_rational:
+        # print("use solve_univariate_inequality")
+        return solve(Equation, Symbol, domain=S.Reals,relational=True)
+    # print("use solve")
+    
+    return solve(Equation, Symbol)
+def Count_comma(Equation_to_Solve):
+    count = 0
+    for i in str(Equation_to_Solve):
+        if str(i) == ',':
+            count += 1
+    return count
+def Solve(Equation_to_Solve):
+    Commas = Count_comma(Equation_to_Solve)
+    equation = ''.join([str(elem) for elem in Equation_to_Solve])
+    if Commas >=4 :
+        return SystemLinearMoreThanTwo(Equation_to_Solve)
+    else:
+        return SolveLeastThan2Symbol(Equation_to_Solve)
